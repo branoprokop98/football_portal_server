616983f9703e935aa6e92f88d86c78168a7e4f40
Branislav <branislavprokop98@gmail.com>
2022-06-12 21:45:44 +0200

    implements methods in league controller

--- src/main/java/sk/home/futportalserver/controllers/LeagueController.java
+++ src/main/java/sk/home/futportalserver/controllers/LeagueController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sk.home.futportalserver.domain.League;
 import sk.home.futportalserver.services.LeagueService;
+import sk.home.futportalserver.services.SeasonService;
 
 import java.util.List;
 
@@ -14,10 +15,12 @@ import java.util.List;
 public class LeagueController {
 
     private final LeagueService leagueService;
+    private final SeasonService seasonService;
 
     @Autowired
-    public LeagueController(LeagueService leagueService) {
+    public LeagueController(LeagueService leagueService, SeasonService seasonService) {
         this.leagueService = leagueService;
+        this.seasonService = seasonService;
     }
 
     @GetMapping("/getAll")
@@ -26,7 +29,17 @@ public class LeagueController {
     }
 
     @GetMapping("/getLeagues/{id}")
-    public List<League> getLeaguesBySeasonId(@PathVariable Long id){
-        return leagueService.getLeaguesBySeasonId(id);
+    public ResponseEntity<List<League>> getLeaguesBySeasonId(@PathVariable Long id){
+        return ResponseEntity.ok().body(leagueService.getLeaguesBySeasonId(id));
+    }
+
+    @GetMapping("/getLeague/{leagueId}")
+    public ResponseEntity<League> getLeagueById(@PathVariable Long leagueId){
+        return ResponseEntity.ok().body(leagueService.getLeagueById(leagueId));
+    }
+
+    @GetMapping("getIdOfLeagueBySeasonAndName/{nameOfLeague}/{year}")
+    public Integer getLeagueId(@PathVariable String nameOfLeague, @PathVariable String year){
+        return  seasonService.getLeagueIdBySeasonYearAndLeagueName(nameOfLeague, year);
     }
 }
